58cae3dc2f315006e7c6901ecadc30974af282f4
Badsha-Nishan <badshanisan14@gmail.com>
2026-02-03 13:10:34 +0600

    added one more fnc

--- index.js
+++ index.js
@@ -124,18 +124,46 @@
  * more than 200: --> 70
  */
 
-function discountPrice(quantity) {
-  let total = 0;
+// function discountPrice(quantity) {
+//   let total = 0;
+//   if (quantity <= 100) {
+//     total = quantity * 100;
+//   } else if (quantity <= 200) {
+//     total = quantity * 90;
+//   } else {
+//     total = quantity * 70;
+//   }
+//   return total;
+// }
+
+// const quantity = 201;
+// const output = discountPrice(quantity);
+// console.log(output);
+
+function layeredDiscountedTotal(quantity) {
+  const first100Price = 100;
+  const second100Price = 90;
+  const above200Price = 70;
   if (quantity <= 100) {
-    total = quantity * 100;
+    const total = first100Price * quantity;
+    return total;
   } else if (quantity <= 200) {
-    total = quantity * 90;
+    const first100Total = first100Price * 100;
+    const remainQty = quantity - 100;
+    const remainingTotal = remainQty * second100Price;
+    const total = first100Total + remainingTotal;
+    return total;
   } else {
-    total = quantity * 70;
+    const first100Total = first100Price * 100;
+    const second100Total = second100Price * 100;
+    const remainingTotal = first100Total + second100Total;
+    const remainQty = quantity - 200;
+    const remainTotal = remainQty * above200Price;
+    const total = remainingTotal + remainTotal;
+    return total;
   }
-  return total;
 }
 
 const quantity = 201;
-const output = discountPrice(quantity);
-console.log(output);
+const result = layeredDiscountedTotal(quantity);
+console.log(result);
